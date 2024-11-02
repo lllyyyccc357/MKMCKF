b62b397eac428ad415a4f0c3a5bc13f4c220ee1b
梁奕宸 <122443216+lllyyyccc357@users.noreply.github.com>
2024-11-02 21:44:58 +0800

    Delete mag_elimination directory

--- mag_elimination/Orientation/Thomas/quaterion_function_scalar4/QuaternionsInv.m
+++
@@ -1,6 +0,0 @@
-function qr=QuaternionsInv(q)
-%QuaternionsInv calculates the quaterion conjucture
-% The structure of the Quaternions is (q_v,q_w).The scalar is at the 4th.
-% q=(x,y,z,w)
-    qr=QuaternionsConj(q)./QuaternionsNorm(q)^2;
-end
