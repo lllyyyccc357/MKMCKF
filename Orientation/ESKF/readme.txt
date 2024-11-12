7e48794bc5b6bf8ca68da96675b7c98cb1684616
梁奕宸 <122443216+lllyyyccc357@users.noreply.github.com>
2024-11-12 17:46:43 +0800

    Delete Orientation directory

--- Orientation/ESKF/readme.txt
+++
@@ -1,1 +0,0 @@
-gn is the gravity negative
--- Orientation/Thomas/quaterion_function_scalar4/QuaternionsConj.m
+++
@@ -1,9 +0,0 @@
-function qr=QuaternionsConj(q)
-%QuaternionsConj calculates the quaterion conjucture
-% The structure of the Quaternions is (q_v,q_w).The scalar is at the 4th.
-% q=(x,y,z,w)
-qr(1, :) = -q(1, :);
-qr(2, :) = -q(2, :);
-qr(3, :) = -q(3, :);
-qr(4, :) = q(4, :);
-end
